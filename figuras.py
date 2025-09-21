2f9ca992944c7535b567b959823f9240c498341c
Augusto <a01826775@tec.mx>
2025-09-21 12:59:23 -0600

    Commit numero dos

--- figuras.py
+++ figuras.py
@@ -4,3 +4,11 @@ def rectangulo(base, altura):
 	area = base * altura
 	perimetro = 2 * (base + altura)
 	return area, perimetro
+def circulo(radio):
+        area = 3.1416 * radio * radio
+        perimetro = 3.1416 * (2*rado)
+        return area, perimetro
+def triangulo(base, altura, lado2, lado3):
+    area = (base * altura) / 2
+    perimetro = base + lado2 + lado3
+    return area, perimetro
